d87cb60505a6569c987f9f0eff4f420b036f3abc
Your Name <you@example.com>
2018-02-24 12:57:47 -0600

    started working on verify card

--- verify_card.py
+++ verify_card.py
@@ -15,12 +15,42 @@ xi.	Send back if verified or not
 """
 
 import authtools.usbtools
+import authtools.cardtools
+import Crypto.PublicKey.RSA
+
+
+##
+#variables#
+decrypt_dir = "/tmp/RAMSPACE/"
+usb_mount = "/mnt/keycard/"
+serial_fname = "serial"
+
+pubkey_name = ".pubkey.pem"
+
+card_container = "ENCRYPTED_CARD"
+card_container_enc = card_container + ".enc"
+card_container_dec = card_container + ".dec"
+##
 
 usb = authtools.usbtools.waitForPlug()
 
 if usb:
     usb.mount()
 
+print(usb.listFiles())
 if authtools.cardtools.detectContainer(usb):
     print("Found Container!")
+    serial_file = open(usb_mount + serial_fname)
+    serial = serial_file.read()
+    print(serial)
 
+#sym_key = authtools.certtools.get_symkey_from_serial(serial):
+
+authtools.cardtools.decryptContainerAES(sym_key,card_container_enc,decrypt_dir + card_container_dec)
+card_pubkey_file = open(decrypt_dir + card_container + '/' + serial + pubkey_name)
+card_pubkey = Crypto.PublicKey.RSA.importKey(card_pubkey_file.read())
+
+serial_encrypted = card_pubkey.encrypt(serial,32)
+
+
+usb.unmount()
