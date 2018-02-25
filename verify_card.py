c8ae68c8534d0cd8e1f295cb5a25aeb38ac353ae
Your Name <you@example.com>
2018-02-24 21:36:23 -0600

    so card verification works but its really ugly. Ill clean it up later.

--- verify_card.py
+++ verify_card.py
@@ -19,6 +19,12 @@ import authtools.cardtools
 import authtools.certtools
 import Crypto.PublicKey.RSA
 import hashlib
+import socket
+import ssl 
+import sys 
+
+
+
 
 ##
 #variables#
@@ -27,10 +33,15 @@ usb_mount = "/mnt/keycard/"
 serial_fname = "serial"
 
 pubkey_name = ".pubkey.pem"
+serial_fname_enc = "serial.enc"
 
 card_container = "ENCRYPTED_CARD.zip"
 card_container_enc = card_container + ".enc"
 card_container_dec = card_container + ".dec"
+
+TCP_IP = '10.0.0.78'
+TCP_PORT = 8888
+BUFFER_SIZE = 1024
 ##
 
 usb = authtools.usbtools.waitForPlug()
@@ -64,6 +75,25 @@ card_pubkey = card_pubkey_file.read()
 print(card_pubkey)
 card_pubkey_obj = Crypto.PublicKey.RSA.importKey(card_pubkey)
 
-#serial_encrypted = card_pubkey.encrypt(serial,32)
+serial_encrypted = card_pubkey_obj.encrypt(serial.encode(encoding='UTF-8'),32)
+serial_encrypted_file = open(decrypt_dir + serial_fname_enc ,'w+')
+serial_encrypted_file.write(str(serial_encrypted))
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+ss = ssl.wrap_socket(s)
+#ss = ssl(s)
+
+print (ss.read(BUFFER_SIZE))
+ss.write(serial.encode())
+response = ss.read(65535)
+print(response)
+ss.write(str(serial_encrypted).encode())
+response = ss.read(65535)
+print(response)
+s.close()
+
+
+serial_encrypted_file.close()
+card_pubkey_file.close()
 usb.unmount()
